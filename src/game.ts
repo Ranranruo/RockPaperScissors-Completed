de80c8b27cb4bf95725a6efb96479045f9c3f53c
Ranranruo <tlsalstjr58@gmail.com>
2025-03-20 18:54:34 +0900

    completed

--- src/game.ts
+++ src/game.ts
@@ -16,7 +16,6 @@ const game = () => {
     const gameIo = io.of("/game");
     gameIo.on("connection", (socket) => {
         socket.on("start", (name) => {
-            console.log(name);
             const idx: number = games.findIndex(game => game.name == name);
             if(idx != -1) {
                 games[idx].people.push({id: socket.id, rps: null});
@@ -34,14 +33,30 @@ const game = () => {
                     const idx: number = games.findIndex(game => game.name == name)
                     games[idx].count -= 1;
                     gameIo.to(name).emit("count", games[idx].count);
+                    if(games[idx].count == 0) {
+                        console.log("a");
+                        gameIo.to(games[idx].people[0].id).emit('result', {you: games[idx].people[0].rps, opponent: games[idx].people[1].rps});
+                        gameIo.to(games[idx].people[1].id).emit('result', {you: games[idx].people[1].rps, opponent: games[idx].people[0].rps});
+                        clearInterval(games[idx].timer);
+                    }
                 }, 1100);
                 games.push(game);
             }
             socket.join(name);
+            socket.on('rps', (rps) => {
+                const idx: number = games.findIndex(game => game);
+                console.log(games[idx]);
+                const peopleIdx = games[idx].people.findIndex(people => people.id == socket.id);
+                games[idx].people[peopleIdx].rps = rps;
+                console.log(games[idx].people[peopleIdx]);
+            });
         });
-        socket.on('rps', (rps) => {
-            const idx: number = games.findIndex(game => game);
+
+
+        socket.on('disconnect', () => {
+            
         });
+        
     });
 
     // const findBySocketId = (socketId) => {
